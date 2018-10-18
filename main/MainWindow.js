ad60fd59e78b4bef821ee4f3ca91601d260649bd
David Lamprecht <davilamprecht@gmail.com>
2018-10-17 22:29:28 -0500

    Save Theme into JSON File
    This will let you save Theme into Theme JSON Files so that they can be loaded at a later point in time

--- main/MainWindow.js
+++ main/MainWindow.js
@@ -1,6 +1,7 @@
 const { remote, ipcRenderer } = require ('electron');
 const {dialog, app} = require('electron').remote;
-const fs = require('fs-extra');
+const fsx = require('fs-extra');
+const fs = require('fs');
 const MessageBuilder = require('./lib/MessageBuilder.js');
 
 let mb = new MessageBuilder('overlay1');
@@ -21,7 +22,57 @@ function setTheme(){
   sendMessage(message);
 }
 
+/*
+ * Let's us save the theme into a json file
+ */
+function saveTheme(){
+    let themeName = document.getElementById('themeName').value;
+    if(themeName == ''){
+        themeName = "untitled";
+    }
+    let path = app.getAppPath() + '/themes/';
+    let nameI = 0;
+    let foundName = false;
+    let theme = JSON.stringify(document.getElementById('theme').value);
+    fs.readdir(path, function(err, items) {
+        while(!foundName){
+            foundName = themeName;
+            for (var i=0; i<items.length; i++) {
+                if(nameI > 0){
+                    foundName = themeName + "_" + nameI ;
+                }
+                if(items[i] == (foundName + ".json")){
+                    foundName = false;
+                    break;
+                }
+            }
+            nameI++;
+        }
+        fs.writeFile(app.getAppPath() + '/themes/'+foundName+ ".json", '{"name":"'+foundName+'", "themeContent":'+theme+'}', function(err) {
+            if(err) {
+                return console.log(err);
+            }
 
+            console.log("The file was saved!");
+        });
+    });
+}
+
+function loadTheme(){
+    let themeName = document.getElementById('themeName').value;
+    if(themeName == ''){
+        themeName = "untitled";
+    }
+    let path = app.getAppPath() + '/themes/';
+    try {
+      if (fs.existsSync(path + themeName + ".json")) {
+        var obj = JSON.parse(fs.readFileSync(path + themeName + ".json", 'utf8'));
+        document.getElementById('theme').value = obj.themeContent;
+      }
+    } catch(err) {
+      console.error(err);
+    }
+}
 
 
 
@@ -44,7 +95,7 @@ function ingestFile(){
         return;
     }
 
-    fs.copy(filePaths[0], app.getAppPath() + '/assets/' + filePaths[0].split('\\').pop().split('/').pop())
+    fsx.copy(filePaths[0], app.getAppPath() + '/assets/' + filePaths[0].split('\\').pop().split('/').pop())
     .then(() => console.log('success!'))
     .catch(err => console.error(err))
 });
